4d08f7cd58daf059a9d33bddf32c87f72a24c48f
haroonkhan4149 <haroon.khan4149@gmail.com>
2024-07-08 19:38:47 +0500

    nextjs-blog-website

---
+++ components/post-items.tsx
@@ -0,0 +1,47 @@
+import { CalendarIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { cn, formatDate } from "@/lib/utils";
+
+//this will fetch the main --- ---- part from mdx and pass the array type obj to page.tsx blog
+
+
+interface PostItemProps {
+    slug: string;
+    title: string;
+    description?: string;
+    date: string;
+  }
+  
+  export function PostItem({
+    slug,
+    title,
+    description,
+    date,
+  }: PostItemProps) {
+    return (
+      <article className="flex flex-col gap-2 border-border border-b py-3">
+        <div>
+          <h2 className="text-2xl font-bold">
+            <Link href={"/" + slug}>{title}</Link>
+          </h2>
+        </div>
+        <div className="max-w-none text-muted-foreground">{description}</div>
+        <div className="flex justify-between items-center">
+          <dl>
+            <dt className="sr-only">Published On</dt>
+            <dd className="text-sm sm:text-base font-medium flex items-center gap-1">
+              <CalendarIcon className="h-4 w-4" />
+              <time dateTime={date}>{formatDate(date)}</time>
+            </dd>
+          </dl>
+          <Link
+            href={"/" + slug}
+            className={cn(buttonVariants({ variant: "link" }), "py-0")}
+          >
+            Read more →
+          </Link>
+        </div>
+      </article>
+    );
+  }
---
+++ config/site.ts
@@ -0,0 +1,16 @@
+
+export const siteConfig={
+    name:"Home",
+    url:"https://example.com",
+    description:"This is my blog website",
+    author:"HaroonKhan",
+    
+    links:{
+        twitter:"https://twitter.com/",
+        github:"https://github.com/",
+        personalSite:"https://www.youtube.com/",
+        linkedin:"https://www.linkedin.com/in/haroon-khan-0570b7234/"
+    }
+}
+
+export type siteConfig =typeof siteConfig
